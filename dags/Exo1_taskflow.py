1a2d749c5c148914fd4c1aee998efa3842f13300
sousou07386-cmyk <sousou07386@gmail.com>
2026-08-27 12:35:49 +0000

    test

--- dags/Exo1_taskflow.py
+++ dags/Exo1_taskflow.py
@@ -1,15 +1,20 @@
 from airflow import DAG
 from datetime import datetime
-from airflow.operators.python import pythonOperator
+from airflow.operators.bash import BashOperator
 
-@DAG(
+with DAG(
     dag_id= 'hello_airflow',
+    start_date=datetime(2026, 8, 27),
+    schedule=None,
+    catchup=False,
+    tag=["Exo1_taskflow"],
+
     
-)
+    
+) as dag:
 
-def hello_airflow():
-    @task
-    def task_a():
-        print("Bonjour")
-        return "Task C result"
-
+    tache1= BashOperator(
+        task_id= BashOperator(
+            task_id="tach1e",
+            bash_commande= "echo'Bonjour depuis Airflow'",
+        )
